30afbdc64c75b4a3f2f58a1bc462f2dcf5df5da3
Axel Bocciarelli <axelboc@gmail.com>
2017-06-25 13:59:17 +1000

    set up nwb and create React shell

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+  render() {
+    return (
+      <div id="app">
+        <div id="canvas">
+          <div className="cell"></div>
+          <div className="cell"></div>
+          <div className="cell"></div>
+          <div className="cell"></div>
+          <div className="cell"></div>
+          <div className="cell"></div>
+          <div className="cell"></div>
+          <div className="cell"></div>
+          <div className="cell"></div>
+          <div className="cell"></div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App
