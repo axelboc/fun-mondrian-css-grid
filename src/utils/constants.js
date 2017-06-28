6bf02c34689785e89794b21b845858369f1a28b0
Axel Bocciarelli <axelboc@gmail.com>
2017-06-28 21:12:54 +1000

    adjust generator configuration

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,8 +4,8 @@ export const RED = '#d31614';
 export const YELLOW = '#f6bf02';
 
 export const GAP = 8 // in pixels
-export const TRACK_TO_GAP_RATIO = 2
-export const MAX_ITERATIONS = 7
+export const TRACK_TO_GAP_RATIO = 5
+export const MAX_ITERATIONS = 5
 
 export const HORIZONTAL = 1
 export const VERTICAL = 2
